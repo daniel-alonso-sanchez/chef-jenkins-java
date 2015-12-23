0ec25b6ce5c1567ad5a2ea377429d1bca473d2dc
dalonsoetg <daniel.alonso@eurotaxglass.com>
2015-12-23 12:00:02 +0100

    added proxy config to knife

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -9,4 +9,4 @@ validation_client_name   "etg-validator"
 validation_key           "#{current_dir}/etg-validator.pem"
 chef_server_url          "https://chef.ubuntu.local:10443/organizations/etg"
 cookbook_path            ["#{current_dir}/../cookbooks"]
-http_proxu 				 "http://192.168.10.145:8080"
+http_proxy 				 "http://192.168.10.145:8080"
