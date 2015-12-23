916a23bc3e464704ce50bd6048b518afb32a9af1
dalonsoetg <daniel.alonso@eurotaxglass.com>
2015-12-23 11:49:45 +0100

    initializing repo for chef

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "masterchef"
+client_key               "#{current_dir}/masterchef.pem"
+validation_client_name   "etg-validator"
+validation_key           "#{current_dir}/etg-validator.pem"
+chef_server_url          "https://chef.ubuntu.local:10443/organizations/etg"
+cookbook_path            ["#{current_dir}/../cookbooks"]
