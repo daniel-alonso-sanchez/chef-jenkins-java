df8e40ef555835f16726c6eda66bba49bce8f6f0
dalonsoetg <daniel.alonso@eurotaxglass.com>
2015-12-29 11:22:43 +0100

    added security

--- cookbooks/jenkins/recipes/_master_package.rb
+++ cookbooks/jenkins/recipes/_master_package.rb
@@ -121,7 +121,26 @@ jenkins_user 'jAdmin' do
   email        'jenkins@etg.com'
   password     '..jenkins'
 end
+jenkins_script 'activate global security' do
+  command <<-EOH.gsub(/^ {4}/, '')
+      import jenkins.model.*
+      import hudson.security.*
+      // Get access to the jenkins instance
+      def instance = Jenkins.getInstance()
+      // Activate clobal seufiry with internal hudsonRealm
+      def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+      instance.setSecurityRealm(hudsonRealm)
+      // Create an admin account
+      hudsonRealm.createAccount("#{node['jenkins']['admin']['name']}", "#{node['jenkins']['admin']['password']}")
+      // Activate matrix seurity and add admin user
+      def strategy = new GlobalMatrixAuthorizationStrategy()
+      strategy.add(Jenkins.ADMINISTER, "#{node['jenkins']['admin']['name']}")
+      instance.setAuthorizationStrategy(strategy)
+      
+      instance.save()
+  EOH
+end
 service 'jenkins' do
   supports status: true, restart: true, reload: true
-  action  [:enable, :start]
+  action  [:enable, :restart]
 end
