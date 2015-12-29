374327688cbc16a4ddac8c7f954f9e952042297f
dalonsoetg <daniel.alonso@eurotaxglass.com>
2015-12-29 14:17:37 +0100

    maven recipe customized

--- cookbooks/maven/templates/default/settings.xml.rb
+++ cookbooks/maven/templates/default/settings.xml.rb
@@ -3,12 +3,5 @@
           xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
           xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
  
-  <proxies>   
-    <proxy>
-      <id>corp-prox</id>
-      <active>true</active>
-      <protocol>http</protocol>   
-    
-    </proxy>    
-  </proxies>
+ 
 </settings>
