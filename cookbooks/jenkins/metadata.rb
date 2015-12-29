5f5001b566b51a673533f37a77a8ae2d276bc6c2
dalonsoetg <daniel.alonso@eurotaxglass.com>
2015-12-29 16:17:55 +0100

    removed learn_chef recipe

--- cookbooks/jenkins/metadata.rb
+++ cookbooks/jenkins/metadata.rb
@@ -11,3 +11,10 @@ recipe 'master', 'Installs a Jenkins master'
 depends 'apt',   '~> 2.0'
 depends 'runit', '~> 1.5'
 depends 'yum',   '~> 3.0'
+depends 'java', '~> 1.36.0'
+depends 'maven', '~> 1.0.2'
+depends 'magic_shell'
+
+
+
+
