71510976a7709b3437a544991e620a13c115ab43
dalonsoetg <daniel.alonso@eurotaxglass.com>
2015-12-30 16:29:28 +0100

    securize recipe modified, in order to perform copy of templates instead of old creation of credentials

--- cookbooks/jenkins/recipes/securize.rb
+++ cookbooks/jenkins/recipes/securize.rb
@@ -1,19 +1,4 @@
-jenkins_script 'activate global security' do
-  command <<-EOH.gsub(/^ {4}/, '')
-      import jenkins.model.*
-      import hudson.security.*
-      // Get access to the jenkins instance
-      def instance = Jenkins.getInstance()
-      // Activate clobal seufiry with internal hudsonRealm
-      def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-      instance.setSecurityRealm(hudsonRealm)
-      // Create an admin account
-      hudsonRealm.createAccount("#{node['jenkins']['admin']['name']}", "#{node['jenkins']['admin']['password']}")
-      // Activate matrix seurity and add admin user
-      def strategy = new GlobalMatrixAuthorizationStrategy()
-      strategy.add(Jenkins.ADMINISTER, "#{node['jenkins']['admin']['name']}")
-      instance.setAuthorizationStrategy(strategy)
-      
-      instance.save()
-  EOH
+template "#{node['jenkins']['master']['home']}/config.xml" do
+    source   'config.xml.erb'
+    mode     '0644'   
 end
