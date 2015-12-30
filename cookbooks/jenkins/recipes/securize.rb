600d2bddadc2ea46b1cb9471bca08006d93edd9d
dalonsoetg <daniel.alonso@eurotaxglass.com>
2015-12-30 16:42:30 +0100

    added user admin configuration

--- cookbooks/jenkins/recipes/securize.rb
+++ cookbooks/jenkins/recipes/securize.rb
@@ -4,3 +4,16 @@ template "#{node['jenkins']['master']['home']}/config.xml" do
     owner 'jenkins'
     group 'jenkins'	
 end
+directory "#{node['jenkins']['master']['home']}/users/admin" do
+  owner 'jenkins'
+  group 'jenkins'
+  mode '0751'
+  recursive true
+  action :create
+end
+template "#{node['jenkins']['master']['home']}/users/admin/config.xml" do
+    source   'jenkins-admin-config.xml.erb'
+    mode     '0644'  
+    owner 'jenkins'
+    group 'jenkins'	
+end
